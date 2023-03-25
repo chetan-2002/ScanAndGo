470df016895dd15a0a8c0caaf0be2580db91a442
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2023-03-26 02:30:10 +0530

    added animations assets

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+const assets = {
+  lottieFiles: {
+    login: require('./login.json'),
+    temporary: require('./temporary.json'),
+    signup: require('./signup.json'),
+    success: require('./success.json'),
+  },
+};
+export default assets;
